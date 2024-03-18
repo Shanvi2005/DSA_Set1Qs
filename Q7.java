4dc3f202824d49f90522b5150d5bb9c51f3bbf80
Shanvi2005 <144417466+Shanvi2005@users.noreply.github.com>
2024-03-18 21:06:14 +0530

    Create Q7.java

---
+++ Q7.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static int[] reverseArray(int n, int []nums) {
+       int [] arr = new int[n];
+       for(int i=0;i<n;i++){
+           arr[i] = nums[n-1-i];
+       }
+       return arr;
+
+    }
+}
